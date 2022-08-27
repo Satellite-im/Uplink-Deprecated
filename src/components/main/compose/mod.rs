a7810d3b4127fde70d76d043a2ce0468287ff48c
RetroPronghorn <retropronghorn@gmail.com>
2022-08-27 01:40:33 -0400

    stuff

--- src/components/main/compose/mod.rs
+++ src/components/main/compose/mod.rs
@@ -6,6 +6,8 @@ pub struct Props {
     tesseract: Tesseract,
 }
 
+pub mod write;
+
 #[allow(non_snake_case)]
 pub fn Compose(cx: Scope<Props>) -> Element {
 
---
+++ src/components/main/compose/write.rs
@@ -0,0 +1,45 @@
+use dioxus::prelude::*;
+use dioxus_heroicons::outline::Shape;
+use sir::global_css;
+
+use crate::components::ui_kit::icon_button::{IconButton, self};
+
+#[derive(Props)]
+pub struct Props<'a> {
+    submit: EventHandler<'a>,
+    keypress: EventHandler<'a>,
+    upload: EventHandler<'a>,
+}
+
+#[allow(non_snake_case)]
+pub fn Write<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
+    global_css! {"
+        .write {
+            display: inline-flex;
+            flex-direction: row;
+        }
+        .button-secondary {
+            background: none;
+            svg {
+                stroke: var(--theme-primary);
+            }
+        }
+    "}
+    cx.render(rsx!{
+        div {
+            class: "write",
+            IconButton {
+                icon: Shape::Plus,
+                onclick: cx.props.upload,
+            },
+            textarea {
+            
+            },
+            IconButton {
+                icon: Shape::ArrowRight,
+                state: icon_button::State::Secondary,
+                onclick: cx.props.submit,
+            },
+        },
+    })
+}
