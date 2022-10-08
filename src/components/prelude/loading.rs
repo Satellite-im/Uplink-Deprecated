fb1bd88c3251cde579822c81899cb9069cc7a903
Matt Wisniewski <infamousvaguerat@gmail.com>
2022-10-07 21:25:44 -0400

    splash2

--- src/components/prelude/loading.rs
+++ src/components/prelude/loading.rs
@@ -1,12 +1,13 @@
+use std::fs::File;
+use dioxus::core::to_owned;
 use dioxus::desktop::use_window;
 use dioxus::router::use_router;
 use dioxus::prelude::*;
-
+use rodio::{source::Source, Decoder, OutputStream};
+use std::io::BufReader;
+use futures::StreamExt;
 use crate::{
-    components::ui_kit::{
-        loader::Loader,
-    },
-    Account, LANGUAGE, WINDOW_SUFFIX_NAME,
+    Account, WINDOW_SUFFIX_NAME,
 };
 
 // Remember: owned props must implement PartialEq!
@@ -18,14 +19,34 @@ pub struct Props {
 #[allow(non_snake_case)]
 pub fn Loading(cx: Scope<Props>) -> Element {
     let window = use_window(&cx);
-    let l = use_atom_ref(&cx, LANGUAGE).read();
+    let loaded = use_state(&cx, || false);
+    let tx: &CoroutineHandle<bool> = use_coroutine(&cx, |mut rx: UnboundedReceiver<bool>| {
+        to_owned![loaded];
+        async move {
+            while let Some(flag) = rx.next().await {
+                if flag {
+                    tokio::time::sleep(std::time::Duration::from_secs(4)).await;
+                    loaded.set(true);
+                    break;
+                }
+            }
+        }
+    });
     std::thread::sleep(std::time::Duration::from_millis(10));
-
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let file = BufReader::new(File::open("extra/uplink.mp3").unwrap());
+    let source = Decoder::new(file).unwrap();
+    if let Err(_e) = stream_handle.play_raw(source.convert_samples()) {
+        std::thread::sleep(std::time::Duration::from_secs(4));
+        //TODO: Do something if it fails to load audio?
+    }
     let multipass = cx.props.account.clone();
     let _account_fetch_status = match multipass.read().get_own_identity() {
         Ok(i) => {
-            window.set_title(&format!("{} - {}", i.username(), WINDOW_SUFFIX_NAME));
-            use_router(&cx).push_route("/main", None, None);
+            if *loaded.get() {
+                window.set_title(&format!("{} - {}", i.username(), WINDOW_SUFFIX_NAME));
+                use_router(&cx).push_route("/main", None, None);
+            }
             false
         }
         Err(_) => {
@@ -35,8 +56,9 @@ pub fn Loading(cx: Scope<Props>) -> Element {
     };
 
     cx.render(rsx! {
-        Loader {
-            text: l.checking_account.clone()
+        img {
+            style: "width: 100%",
+            src: "extra/uplink.gif"
         }
     })
 }
