edf08c18136ca51d3bea150148513d43454e2a5c
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2022-10-18 16:45:43 -0400

    bugfix: path to icon for windows build

--- build.rs
+++ build.rs
@@ -17,7 +17,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         "LegalCopyright",
         "Creative Commons Attribution-NonCommercial 1.0",
     );
-    res.set_icon("../extra/windows/uplink.ico");
+    res.set_icon("./extra/windows/uplink.ico");
     res.compile()
         .expect("Failed to run the Windows resource compiler (rc.exe)");
 
