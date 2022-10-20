ace95cee41c4bbc378db8bccfcb5d68ed0b6af3f
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2022-10-20 10:17:32 -0400

    use page_state to render sidebar

--- src/components/main/sidebar/mod.rs
+++ src/components/main/sidebar/mod.rs
@@ -34,7 +34,6 @@ pub fn Sidebar(cx: Scope<Props>) -> Element {
 
     let show_friends = use_state(&cx, || false);
     let show_profile = use_state(&cx, || false);
-    let show_settings = use_state(&cx, || false);
     let state = use_atom_ref(&cx, STATE);
 
     let l = use_atom_ref(&cx, LANGUAGE).read();
@@ -45,141 +44,137 @@ pub fn Sidebar(cx: Scope<Props>) -> Element {
     let chatsdString = l.chats.to_string();
     let has_chats = !state.read().chats.clone().is_empty();
 
-    match cx.props.page_state.get() {
-        PageState::Normal => {
-            // todo: render normally
-        }
-        PageState::Settings => {
-            // todo: render settings
-        }
-    }
-
-    cx.render(rsx!{
+    cx.render(rsx! {
         div {
             class: "sidebar",
-            IconInput {
-                icon: Shape::Search,
-                placeholder: String::from("Search"),
-                value: String::from(""),
-                on_change: move |_| {},
-                on_enter: move |_| {},
-            },
-            config.developer.developer_mode.then(|| rsx! {
-                ExtensionPlaceholder {},
-            })
-            label {
-                "{favString}"
-            },
-            div {
-                class: "favorites",
-                div {
-                    class: "labeled",
-                    IconButton {
-                        icon: Shape::Plus,
-                        on_pressed: move |_| {},
-                    },
-                    span {
-                        "{newchatdString}"
+            match cx.props.page_state.get() {
+                PageState::Settings => rsx! {
+                    Settings {
+                        account: cx.props.account.clone(),
+                        on_hide: move |_| {
+                            cx.props.page_state.set(PageState::Normal);
+                        },
                     }
                 },
-            },
-            label {
-                style: "margin-bottom: 0;",
-                "{chatsdString}"
-            },
-            if has_chats {
-                rsx!(
+                PageState::Normal => rsx! {
+                    IconInput {
+                        icon: Shape::Search,
+                        placeholder: String::from("Search"),
+                        value: String::from(""),
+                        on_change: move |_| {},
+                        on_enter: move |_| {},
+                    },
+                    config.developer.developer_mode.then(|| rsx! {
+                        ExtensionPlaceholder {},
+                    })
+                    label {
+                        "{favString}"
+                    },
                     div {
-                        class: "chat_wrap",
+                        class: "favorites",
                         div {
-                            class: "gradient_mask"
+                            class: "labeled",
+                            IconButton {
+                                icon: Shape::Plus,
+                                on_pressed: move |_| {},
+                            },
+                            span {
+                                "{newchatdString}"
+                            }
                         },
-                        div {
-                            class: "gradient_mask is_bottom"
-                        },
-                        div {
-                            class: "chats",
-                            state.read().chats.iter().rev().map(|conv| {
-                                let conversation = conv.clone();
-                                rsx!(
-                                    chat::Chat {
-                                        account: cx.props.account.clone(),
-                                        conversation: conversation.clone(),
-                                        on_pressed: move |_| {
-                                            state.write().dispatch(Actions::ChatWith(conversation.clone())).save();
-                                        }
-                                    }
-                                )
-                            })
+                    },
+                    label {
+                        style: "margin-bottom: 0;",
+                        "{chatsdString}"
+                    },
+                    if has_chats {
+                        rsx!(
+                            div {
+                                class: "chat_wrap",
+                                div {
+                                    class: "gradient_mask"
+                                },
+                                div {
+                                    class: "gradient_mask is_bottom"
+                                },
+                                div {
+                                    class: "chats",
+                                    state.read().chats.iter().rev().map(|conv| {
+                                        let conversation = conv.clone();
+                                        rsx!(
+                                            chat::Chat {
+                                                account: cx.props.account.clone(),
+                                                conversation: conversation.clone(),
+                                                on_pressed: move |_| {
+                                                    state.write().dispatch(Actions::ChatWith(conversation.clone())).save();
+                                                }
+                                            }
+                                        )
+                                    })
+                                }
+                            }
+                        )
+                    } else {
+                        rsx!(
+                            p {
+                                "{noactivechatdString}"
+                            },
+                            div {
+                                class: "m-bottom"
+                            },
+                            Button {
+                                icon: Shape::Plus,
+                                text: l.start_one.to_string(),
+                                on_pressed: move |_| show_friends.set(true),
+                            },
+                        )
+                    },
+                    (**show_friends).then(|| rsx!{
+                        //TODO: this is a fix for now, but next milestone we should rework popups to
+                        // de-render themselves after css hide animations are completed.
+                        Friends {
+                            account: cx.props.account.clone(),
+                            messaging: cx.props.messaging.clone(),
+                            title: friendString,
+                            show: true,
+                            icon: Shape::Users,
+                            on_hide: move |_| {
+                                show_friends.set(false);
+                            },
                         }
-                    }
-                )
-            } else {
-                rsx!(
-                    p {
-                        "{noactivechatdString}"
+                    }),
+                    Profile {
+                        account: cx.props.account.clone(),
+                        show: *show_profile.clone(),
+                        on_hide: move |_| show_profile.set(false),
                     },
-                    div {
-                        class: "m-bottom"
-                    },
-                    Button {
-                        icon: Shape::Plus,
-                        text: l.start_one.to_string(),
-                        on_pressed: move |_| show_friends.set(true),
-                    },
-                )
-            },
-            (**show_friends).then(|| rsx!{
-                //TODO: this is a fix for now, but next milestone we should rework popups to
-                // de-render themselves after css hide animations are completed.
-                Friends {
-                    account: cx.props.account.clone(),
-                    messaging: cx.props.messaging.clone(),
-                    title: friendString,
-                    show: true,
-                    icon: Shape::Users,
-                    on_hide: move |_| {
-                        show_friends.set(false);
-                    },
-                }
-            }),
-            Profile {
-                account: cx.props.account.clone(),
-                show: *show_profile.clone(),
-                on_hide: move |_| show_profile.set(false),
-            },
-            // todo: move this out
-            (**show_settings).then(|| rsx!{
-                Settings {
-                    account: cx.props.account.clone(),
-                    on_hide: move |_| {
-                        //show_settings.set(false);
-                        cx.props.page_state.set(PageState::Normal);
-                    },
-                },
-            }),
-            Nav {
-                account: cx.props.account.clone(),
-                on_pressed: move | e: NavEvent | {
-                    show_friends.set(false);
-                    show_profile.set(false);
-                    show_settings.set(false);
+                    Nav {
+                        account: cx.props.account.clone(),
+                        on_pressed: move | e: NavEvent | {
+                            show_friends.set(false);
+                            show_profile.set(false);
+        
+                            match e {
+                                NavEvent::Home => {
+                                },
+                                NavEvent::Files => {
+                                },
+                                NavEvent::Friends => {
+                                    show_friends.set(true);
+                                },
+                                NavEvent::Profile => {
+                                    show_profile.set(true);
+                                },
+                                NavEvent::Settings => {
+                                },
+                            };
 
-                    match e {
-                        NavEvent::Home => {
-                        },
-                        NavEvent::Files => {
-                        },
-                        NavEvent::Friends => {
-                            show_friends.set(true);
-                        },
-                        NavEvent::Profile => {
-                            show_profile.set(true);
-                        },
-                        NavEvent::Settings => {
-                            //show_settings.set(true);
-                            cx.props.page_state.set(PageState::Settings);
-                        },
+                            if let NavEvent::Settings = e {
+                                cx.props.page_state.set(PageState::Settings);
+                            } else {
+                                cx.props.page_state.set(PageState::Normal);
+                            }
+                        }
                     }
                 }
             }
