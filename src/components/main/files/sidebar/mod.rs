e62192013171766250f72cc30f6c606efb4693fd
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2022-10-31 13:42:45 -0400

    chore (files sidebar): improve example

--- src/components/main/files/sidebar/mod.rs
+++ src/components/main/files/sidebar/mod.rs
@@ -111,22 +111,34 @@ pub fn Folder(cx: Scope, dir: Directory) -> Element {
 #[inline_props]
 #[allow(non_snake_case)]
 pub fn Sidebar(cx: Scope, _account: crate::Account) -> Element {
+    // TODO: This should be generated based on actual content later.
+    // We will create reusable components for a folder and just pass children as data and render
+    // recursively automatically.
+    // This is just to work on css
     let subdir_1 = Directory {
         name: "Subdir1".into(),
         contents: vec![Box::new(DirItem::File(File { name: "f1".into() }))],
+    };
+
+    let subdir_3 = Directory {
+        name: "Subdir3".into(),
+        contents: vec![Box::new(DirItem::File(File { name: "f2".into() }))],
+    };
+    let subdir_2 = Directory {
+        name: "Subdir2".into(),
+        contents: vec![Box::new(DirItem::Directory(subdir_3))],
     };
     let directory = Directory {
         name: "Folder 1".into(),
         contents: vec![
             Box::new(DirItem::Directory(subdir_1)),
-            Box::new(DirItem::File(File { name: "f2".into() })),
+            Box::new(DirItem::Directory(subdir_2)),
+            Box::new(DirItem::File(File { name: "f3".into() })),
         ],
     };
+
+    // if multiple folders are desired under `Files`, this could render a list of `DirEntry`
     cx.render(rsx! {
-        // TODO: This should be generated based on actual content later.
-        // We will create reusable components for a folder and just pass children as data and render
-        // recursively automatically.
-        // This is just to work on css
         div {
             id: "sidebar",
             class: "tree noselect",
@@ -154,198 +166,3 @@ pub fn Sidebar(cx: Scope, _account: crate::Account) -> Element {
         }
     })
 }
-
-/*
-#[allow(non_snake_case)]
-pub fn Sidebar(cx: Scope<Props>) -> Element {
-    cx.render(rsx! {
-        // TODO: This should be generated based on actual content later.
-        // We will create reusable components for a folder and just pass children as data and render
-        // recursively automatically.
-        // This is just to work on css
-        div {
-            id: "sidebar",
-            class: "tree noselect",
-            Usage {
-                usage: UsageStats {
-                    available: 1256,
-                    total: 123456,
-                    used: 122200,
-                    percent_free: 75,
-                }
-            },
-            label {
-                class: "m-top-sm",
-                "Files"
-            },
-            div {
-                class: "tree_wrapper",
-                label {
-                    class: "tree_folder root",
-                    div {
-                        class: "row",
-                        Icon {
-                            class: "closed",
-                            icon: Shape::Folder,
-                        },
-                        Icon {
-                            class: "open",
-                            icon: Shape::FolderOpen,
-                        },
-                        "Folder 1"
-                    },
-                    input {
-                        id: "tree_folder_control",
-                        "type": "checkbox",
-                    },
-                    label {
-                        class: "tree_folder",
-                        div {
-                            class: "row",
-                            Icon {
-                                class: "closed",
-                                icon: Shape::Folder,
-                            },
-                            Icon {
-                                class: "open",
-                                icon: Shape::FolderOpen,
-                            },
-                            "SubFolder 1"
-                        },
-                        input {
-                            id: "tree_folder_control",
-                            "type": "checkbox",
-                        },
-                        a {
-                            class: "tree_item",
-                            div {
-                                class: "row",
-                                Icon {
-                                    icon: Shape::Document,
-                                },
-                                "Item"
-                            },
-                        },
-                    },
-                    label {
-                        class: "tree_folder",
-                        div {
-                            class: "row",
-                            Icon {
-                                class: "closed",
-                                icon: Shape::Folder,
-                            },
-                            Icon {
-                                class: "open",
-                                icon: Shape::FolderOpen,
-                            },
-                            "Subfolder 2",
-                        },
-                        input {
-                            id: "tree_folder_control",
-                            "type": "checkbox",
-                        },
-                        label {
-                            class: "tree_folder",
-                            div {
-                                class: "row",
-                                Icon {
-                                    class: "closed",
-                                    icon: Shape::Folder,
-                                },
-                                Icon {
-                                    class: "open",
-                                    icon: Shape::FolderOpen,
-                                },
-                                "Subfolder 1",
-                            },
-                            input {
-                                id: "tree_folder_control",
-                                "type": "checkbox",
-                            },
-                            label {
-                                class: "tree_folder",
-                                div {
-                                    class: "row",
-                                    Icon {
-                                        class: "closed",
-                                        icon: Shape::Folder,
-                                    },
-                                    Icon {
-                                        class: "open",
-                                        icon: Shape::FolderOpen,
-                                    },
-                                    "Subfolder 2",
-                                },
-                                input {
-                                    id: "tree_folder_control",
-                                    "type": "checkbox",
-                                },
-                                a {
-                                    class: "tree_item",
-                                    div {
-                                        class: "row",
-                                        Icon {
-                                            icon: Shape::Document,
-                                        },
-                                        "Item"
-                                    },
-                                },
-                            }
-                            a {
-                                class: "tree_item",
-                                div {
-                                    class: "row",
-                                    Icon {
-                                        icon: Shape::Document,
-                                    },
-                                    "Item"
-                                },
-                            },
-                        }
-                        a {
-                            class: "tree_item",
-                            div {
-                                class: "row",
-                                Icon {
-                                    icon: Shape::Document,
-                                },
-                                "Item"
-                            },
-                        },
-                    }
-                },
-                label {
-                    class: "tree_folder root",
-                    div {
-                        class: "row",
-                        Icon {
-                            class: "closed",
-                            icon: Shape::Folder,
-                        },
-                        Icon {
-                            class: "open",
-                            icon: Shape::FolderOpen,
-                        },
-                        "Folder 2"
-                    },
-                    input {
-                        id: "tree_folder_control",
-                        "type": "checkbox",
-                    },
-                    a {
-                        class: "tree_item",
-                        div {
-                            class: "row",
-                            Icon {
-                                icon: Shape::Document,
-                            },
-                            "Item"
-                        },
-                    },
-                },
-            }
-        }
-    })
-}
-*/
