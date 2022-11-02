e225628e423310095e57502133cfb3bfbf0edab6
Calogero Cimino <calogerocimino@Calogeros-MBP.station>
2022-11-02 14:50:07 +0100

    added sound notification utility

---
+++ src/utils/sounds.rs
@@ -0,0 +1,21 @@
+use soloud::*;
+
+pub enum Sounds {
+    Notification,
+    General,
+}
+
+pub fn play (sound: Sounds) {
+
+    let sl = Soloud::default().unwrap();
+    let mut wav = audio::Wav::default();
+     match sound {
+        Sounds::Notification => wav.load_mem(include_bytes!("../../extra/assets/sounds/Ponderous.ogg")).unwrap(),
+        Sounds::General => {}, 
+
+    };
+    sl.play(&wav);
+    while sl.voice_count() > 0 {
+        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
+}
