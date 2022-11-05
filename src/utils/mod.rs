dab0fec94220085f61d97b7cebee16a32cf74a19
InfamousVague <infamousvaguerat@gmail.com>
2022-11-05 17:22:07 +0000

    fix mismatch between markdown inline and in messages

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -44,7 +44,6 @@ pub fn get_pfp_from_did(did: DID, mp: &Account) -> String {
 }
 
 pub fn wrap_in_markdown(val: &str) -> String {
-    println!("{}", val);
     let replace_newlines = val.clone().replace("\n", "<br />");
 
     let regex_asterisk = Regex::new(r"\*{2}(.*?)\*{2}").unwrap();
@@ -52,13 +51,12 @@ pub fn wrap_in_markdown(val: &str) -> String {
     let regex_one_underscore = Regex::new(r"_{1}(.*?)_{1}").unwrap();
     let regex_two_underscores_first = Regex::new(r"_{2}(.*?)_{2}").unwrap();
     let regex_two_underscores_second = Regex::new(r"\){3}(.*?)\({3}").unwrap();
-
     
     let replacement_asterisk = "<span class=\"delimiter\">**</span><b>$1</b><span class=\"delimiter\">**</span>";
     let replacement_tilda = "<span class=\"delimiter\">~~</span><strike>$1</strike><span class=\"delimiter\">~~</span>";
     let replacement_one_underscore = "<span class=\"delimiter\">_</span><i>$1</i><span class=\"delimiter\">_</span>";
     let replacement_two_underscore_first = ")))$1(((";
-    let replacement_two_underscore_second = "<span class=\"delimiter\">__</span><u>$1</u><span class=\"delimiter\">__</span>";
+    let replacement_two_underscore_second = "<span class=\"delimiter\">__</span><b>$1</b><span class=\"delimiter\">__</span>";
 
     let final_string = regex_asterisk.replace_all(replace_newlines.as_str(), replacement_asterisk);
     let final_string = regex_tilda.replace_all(&final_string, replacement_tilda);
