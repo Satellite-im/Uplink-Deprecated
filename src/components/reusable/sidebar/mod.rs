753a4a0f6802dd6c05a2124f200e14c913007dd1
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2022-11-09 15:23:59 -0500

    feat (reusable): add resuable components for sidebar and nav

---
+++ src/components/reusable/sidebar/mod.rs
@@ -0,0 +1,39 @@
+use dioxus::prelude::*;
+
+use crate::{
+    components::{
+        reusable::nav::{Nav, NavEvent},
+        ui_kit::extension_placeholder::ExtensionPlaceholder,
+    },
+    utils::config::Config,
+    Account,
+};
+
+#[derive(Props)]
+pub struct Props<'a> {
+    account: Account,
+    active: NavEvent,
+    children: Element<'a>,
+}
+
+#[allow(non_snake_case)]
+pub fn Sidebar<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
+    let config = Config::load_config_or_default();
+
+    cx.render(rsx! {
+        div {
+            id: "sidebar app-sidebar",
+            config.developer.developer_mode.then(|| rsx! {
+                ExtensionPlaceholder {},
+            }),
+            div {
+                class: "children sidebar-content",
+                &cx.props.children
+            },
+            Nav {
+                account: cx.props.account.clone(),
+                active: cx.props.active.clone(),
+            }
+        }
+    })
+}
