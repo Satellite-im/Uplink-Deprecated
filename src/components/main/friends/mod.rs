699369e0267d5312bd1103572cfd7dce0f2463e3
lgmarchi <lucasgmarchi@gmail.com>
2022-11-11 20:20:08 -0300

    Sort friends list

--- src/components/main/friends/mod.rs
+++ src/components/main/friends/mod.rs
@@ -8,10 +8,8 @@ use crate::{
 };
 
 use dioxus::prelude::*;
-use std::{collections::HashSet, time::Duration};
 use warp::multipass::Friends;
 
-#[derive(PartialEq)]
 struct UsernameAndDID {
     username: String, 
     did: warp::crypto::DID,
@@ -26,38 +24,15 @@ pub struct Props {
 #[allow(non_snake_case)]
 pub fn Friends(cx: Scope<Props>) -> Element {
     let add_error = use_state(&cx, String::new);
-    let friends = use_state(&cx, || {
-        Vec::new()
-    });
-     let friends_did = use_state(&cx, || {
-        HashSet::from_iter(cx.props.account.list_friends().unwrap_or_default())
-    });
+    let friend_did_list = cx.props.account.list_friends().unwrap_or_default();
 
-    use_future(
-        &cx,
-        (friends, &cx.props.account.clone(), friends_did),
-        |(friends, mp, friends_did)| async move {
-            
-            loop {
-                let friends_list: HashSet<_> =
-                HashSet::from_iter(mp.read().list_friends().unwrap_or_default());
-
-                if *friends_did != friends_list {
-                    let mut username_did_vector: Vec<UsernameAndDID> = Vec::new();
-                    for friend_did in friends_list {
-                        let _friend_username = utils::get_username_from_did(friend_did.clone(), &mp);
-                        let _friend = UsernameAndDID {username: _friend_username, did: friend_did.clone()};
-                        username_did_vector.push(_friend);
-                        tokio::time::sleep(Duration::from_millis(300)).await;
-                    }
-                    username_did_vector.sort_by(|a, b| a.username.to_lowercase().cmp(&b.username.to_lowercase()));
-                    friends.set(username_did_vector);
-                }
-          
-                tokio::time::sleep(Duration::from_millis(300)).await;
-            }
-        },
-    );
+    let mut username_did_vector: Vec<UsernameAndDID> = Vec::new();
+    for friend_did in friend_did_list.iter() {
+        let _friend_username = utils::get_username_from_did(friend_did.clone(), &cx.props.account.clone());
+        let _friend = UsernameAndDID {username: _friend_username, did: friend_did.clone()};
+        username_did_vector.push(_friend);
+    }
+    username_did_vector.sort_by(|a, b| a.username.to_lowercase().cmp(&b.username.to_lowercase()));
 
     cx.render(rsx! {
         div {
@@ -67,7 +42,7 @@ pub fn Friends(cx: Scope<Props>) -> Element {
                 id: "content",
                 div {
                     class: "friends-list",
-                    friends.iter().map(|user| rsx!(
+                    username_did_vector.iter().map(|user| rsx!(
                         Friend {
                             account: cx.props.account.clone(),
                             messaging: cx.props.messaging.clone(),
