93e7a3570ffc9d3fa4d9f85c2b7c0d3fbede8d88
lgmarchi <lucasgmarchi@gmail.com>
2022-11-11 20:06:37 -0300

    Adjusting friend list (WIP)

--- src/components/main/friends/mod.rs
+++ src/components/main/friends/mod.rs
@@ -4,12 +4,18 @@ pub mod sidebar;
 
 use crate::{
     components::main::friends::{friend::Friend, sidebar::Sidebar},
-    Account, Messaging,
+    utils, Account, Messaging,
 };
 
 use dioxus::prelude::*;
 use std::{collections::HashSet, time::Duration};
 use warp::multipass::Friends;
+
+#[derive(PartialEq)]
+struct UsernameAndDID {
+    username: String, 
+    did: warp::crypto::DID,
+}
 
 #[derive(Props, PartialEq)]
 pub struct Props {
@@ -21,21 +27,33 @@ pub struct Props {
 pub fn Friends(cx: Scope<Props>) -> Element {
     let add_error = use_state(&cx, String::new);
     let friends = use_state(&cx, || {
+        Vec::new()
+    });
+     let friends_did = use_state(&cx, || {
         HashSet::from_iter(cx.props.account.list_friends().unwrap_or_default())
     });
 
     use_future(
         &cx,
-        (friends, &cx.props.account.clone()),
-        |(friends, mp)| async move {
+        (friends, &cx.props.account.clone(), friends_did),
+        |(friends, mp, friends_did)| async move {
+            
             loop {
                 let friends_list: HashSet<_> =
-                    HashSet::from_iter(mp.read().list_friends().unwrap_or_default());
+                HashSet::from_iter(mp.read().list_friends().unwrap_or_default());
 
-                if *friends != friends_list {
-                    friends.set(friends_list);
+                if *friends_did != friends_list {
+                    let mut username_did_vector: Vec<UsernameAndDID> = Vec::new();
+                    for friend_did in friends_list {
+                        let _friend_username = utils::get_username_from_did(friend_did.clone(), &mp);
+                        let _friend = UsernameAndDID {username: _friend_username, did: friend_did.clone()};
+                        username_did_vector.push(_friend);
+                        tokio::time::sleep(Duration::from_millis(300)).await;
+                    }
+                    username_did_vector.sort_by(|a, b| a.username.to_lowercase().cmp(&b.username.to_lowercase()));
+                    friends.set(username_did_vector);
                 }
-
+          
                 tokio::time::sleep(Duration::from_millis(300)).await;
             }
         },
@@ -53,7 +71,7 @@ pub fn Friends(cx: Scope<Props>) -> Element {
                         Friend {
                             account: cx.props.account.clone(),
                             messaging: cx.props.messaging.clone(),
-                            friend: user.clone(),
+                            friend: user.did.clone(),
                             on_chat: move |_| {
                                 add_error.set("".into());
                                 use_router(&cx).push_route("/main", None, None);
