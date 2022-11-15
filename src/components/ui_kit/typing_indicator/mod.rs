b1a26fafd2ebf7b77896ee9e3ddbca40cc4a1461
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2022-11-15 12:21:55 -0500

    fix (clippy)

--- src/components/ui_kit/typing_indicator/mod.rs
+++ src/components/ui_kit/typing_indicator/mod.rs
@@ -8,21 +8,17 @@ pub struct Props {
 #[allow(non_snake_case)]
 pub fn TypingIndicator(cx: Scope<Props>) -> Element {
     let users_list = cx.props.users.clone();
-    let name_typing = 
-    if users_list.len() <= 3 { 
-        users_list.join(", ") 
-    } else { 
-        String::from(users_list.len().to_string() + " users") 
+    let name_typing = if users_list.len() <= 3 {
+        users_list.join(", ")
+    } else {
+        users_list.len().to_string() + " users"
     };
-    let article = 
-    if users_list.is_empty() { 
-        String::from("Why do i see this indicator? None is") 
-    } 
-    else if users_list.len() == 1 { 
-        String::from(" is") 
-    } 
-    else { 
-        String::from(" are") 
+    let article = if users_list.is_empty() {
+        String::from("Why do i see this indicator? None is")
+    } else if users_list.len() == 1 {
+        String::from(" is")
+    } else {
+        String::from(" are")
     };
 
     cx.render(rsx! {
