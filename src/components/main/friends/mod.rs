07989bdf11c6dbf07f4c8634377885eaba7daae1
lgmarchi <lucasgmarchi@gmail.com>
2022-11-16 16:02:41 -0300

    Improve code

--- src/components/main/friends/mod.rs
+++ src/components/main/friends/mod.rs
@@ -10,7 +10,6 @@ use crate::{
 };
 
 use dioxus::prelude::*;
-use warp::multipass::Friends;
 
 #[derive(Clone, PartialEq)]
 struct UsernameAndDID {
@@ -35,9 +34,7 @@ pub fn Friends(cx: Scope<Props>) -> Element {
     log::debug!("rendering Friends");
     let add_error = use_state(&cx, String::new);
     let friends_grouped_per_first_letter = use_state(&cx, || Vec::new());
-    let friends = use_state(&cx, || {
-        HashSet::from_iter(cx.props.account.list_friends().unwrap_or_default())
-    });
+    let friends = use_state(&cx, || HashSet::new());
 
     use_future(
         &cx,
@@ -51,16 +48,14 @@ pub fn Friends(cx: Scope<Props>) -> Element {
                 let friends_list: HashSet<_> =
                     HashSet::from_iter(mp.read().list_friends().unwrap_or_default());
 
-                if *friends != friends_list {
+                if *friends != friends_list && friends_list.len() > 0 {
                     log::debug!("updating friends list ");
+                    let new_friends_list = order_friend_list(&friends_list, &mp);
+                    friends_grouped_per_first_letter.set(new_friends_list);
                     friends.set(friends_list);
                 }
-                if friends.len() > 0 {
-                    let new_friends_list = order_friend_list(&friends, &mp);
-                    friends_grouped_per_first_letter.set(new_friends_list);
-                }
 
-                tokio::time::sleep(Duration::from_millis(1000)).await;
+                tokio::time::sleep(Duration::from_millis(300)).await;
             }
         },
     );
@@ -74,12 +69,13 @@ pub fn Friends(cx: Scope<Props>) -> Element {
                 div {
                     class: "friends-list",
                     friends_grouped_per_first_letter.iter().map(|friends_per_char_list| {
+                        let first_username_char = friends_per_char_list.first_letter_friends;
                         rsx!(
                             div {
                                 class: "friends-separator",
                                 h5 {
-                                    id: "{friends_per_char_list.first_letter_friends}",
-                                    "{friends_per_char_list.first_letter_friends}"
+                                    id: "{first_username_char}",
+                                    "{first_username_char}"
                                 }
                             }
                             friends_per_char_list.friends.iter().map(|user| {
@@ -103,32 +99,32 @@ pub fn Friends(cx: Scope<Props>) -> Element {
                }
             }
             ul {
-                li { a { href: "#A", prevent_default: "onclick", rel: "noopener noreferrer", "A", } }
-                li { a { href: "#B", prevent_default: "onclick", rel: "noopener noreferrer", "B", } }
-                li { a { href: "#C", prevent_default: "onclick", rel: "noopener noreferrer", "C", } }
-                li { a { href: "#D", prevent_default: "onclick", rel: "noopener noreferrer", "D", } }
-                li { a { href: "#E", prevent_default: "onclick", rel: "noopener noreferrer", "E", } }
-                li { a { href: "#F", prevent_default: "onclick", rel: "noopener noreferrer", "F", } }
-                li { a { href: "#G", prevent_default: "onclick", rel: "noopener noreferrer", "G", } }
-                li { a { href: "#H", prevent_default: "onclick", rel: "noopener noreferrer", "H", } }
-                li { a { href: "#I", prevent_default: "onclick", rel: "noopener noreferrer", "I", } }
-                li { a { href: "#J", prevent_default: "onclick", rel: "noopener noreferrer", "J", } }
-                li { a { href: "#K", prevent_default: "onclick", rel: "noopener noreferrer", "K", } }
-                li { a { href: "#L", prevent_default: "onclick", rel: "noopener noreferrer", "L", } }
-                li { a { href: "#M", prevent_default: "onclick", rel: "noopener noreferrer", "M", } }
-                li { a { href: "#N", prevent_default: "onclick", rel: "noopener noreferrer", "N", } }
-                li { a { href: "#O", prevent_default: "onclick", rel: "noopener noreferrer", "O", } }
-                li { a { href: "#P", prevent_default: "onclick", rel: "noopener noreferrer", "P", } }
-                li { a { href: "#Q", prevent_default: "onclick", rel: "noopener noreferrer", "Q", } }
-                li { a { href: "#R", prevent_default: "onclick", rel: "noopener noreferrer", "R", } }
-                li { a { href: "#S", prevent_default: "onclick", rel: "noopener noreferrer", "S", } }
-                li { a { href: "#T", prevent_default: "onclick", rel: "noopener noreferrer", "T", } }
-                li { a { href: "#U", prevent_default: "onclick", rel: "noopener noreferrer", "U", } }
-                li { a { href: "#V", prevent_default: "onclick", rel: "noopener noreferrer", "V", } }
-                li { a { href: "#W", prevent_default: "onclick", rel: "noopener noreferrer", "W", } }
-                li { a { href: "#X", prevent_default: "onclick", rel: "noopener noreferrer", "X", } }
-                li { a { href: "#Y", prevent_default: "onclick", rel: "noopener noreferrer", "Y", } }
-                li { a { href: "#Z", prevent_default: "onclick", rel: "noopener noreferrer", "Z", } }
+                    li { a { href: "#A", prevent_default: "onclick", rel: "noopener noreferrer", "A", } }
+                    li { a { href: "#B", prevent_default: "onclick", rel: "noopener noreferrer", "B", } }
+                    li { a { href: "#C", prevent_default: "onclick", rel: "noopener noreferrer", "C", } }
+                    li { a { href: "#D", prevent_default: "onclick", rel: "noopener noreferrer", "D", } }
+                    li { a { href: "#E", prevent_default: "onclick", rel: "noopener noreferrer", "E", } }
+                    li { a { href: "#F", prevent_default: "onclick", rel: "noopener noreferrer", "F", } }
+                    li { a { href: "#G", prevent_default: "onclick", rel: "noopener noreferrer", "G", } }
+                    li { a { href: "#H", prevent_default: "onclick", rel: "noopener noreferrer", "H", } }
+                    li { a { href: "#I", prevent_default: "onclick", rel: "noopener noreferrer", "I", } }
+                    li { a { href: "#J", prevent_default: "onclick", rel: "noopener noreferrer", "J", } }
+                    li { a { href: "#K", prevent_default: "onclick", rel: "noopener noreferrer", "K", } }
+                    li { a { href: "#L", prevent_default: "onclick", rel: "noopener noreferrer", "L", } }
+                    li { a { href: "#M", prevent_default: "onclick", rel: "noopener noreferrer", "M", } }
+                    li { a { href: "#N", prevent_default: "onclick", rel: "noopener noreferrer", "N", } }
+                    li { a { href: "#O", prevent_default: "onclick", rel: "noopener noreferrer", "O", } }
+                    li { a { href: "#P", prevent_default: "onclick", rel: "noopener noreferrer", "P", } }
+                    li { a { href: "#Q", prevent_default: "onclick", rel: "noopener noreferrer", "Q", } }
+                    li { a { href: "#R", prevent_default: "onclick", rel: "noopener noreferrer", "R", } }
+                    li { a { href: "#S", prevent_default: "onclick", rel: "noopener noreferrer", "S", } }
+                    li { a { href: "#T", prevent_default: "onclick", rel: "noopener noreferrer", "T", } }
+                    li { a { href: "#U", prevent_default: "onclick", rel: "noopener noreferrer", "U", } }
+                    li { a { href: "#V", prevent_default: "onclick", rel: "noopener noreferrer", "V", } }
+                    li { a { href: "#W", prevent_default: "onclick", rel: "noopener noreferrer", "W", } }
+                    li { a { href: "#X", prevent_default: "onclick", rel: "noopener noreferrer", "X", } }
+                    li { a { href: "#Y", prevent_default: "onclick", rel: "noopener noreferrer", "Y", } }
+                    li { a { href: "#Z", prevent_default: "onclick", rel: "noopener noreferrer", "Z", } }
                 }
             }
     })
@@ -142,6 +138,7 @@ fn order_friend_list(
     let mut group_of_friends_with_same_first_username_letter: Vec<UsernameAndDID> = Vec::new();
     let mut friends_grouped_per_first_letter: Vec<FriendListAlpha> = Vec::new();
 
+    // Get all friends username
     for friend_did in friend_did_list.iter() {
         let _friend_username = utils::get_username_from_did(friend_did.clone(), account);
         let _friend_username_and_did = UsernameAndDID {
@@ -153,6 +150,7 @@ fn order_friend_list(
 
     username_did.sort_by(|a, b| a.username.cmp(&b.username));
 
+    // Get the first letter username that start the list
     let mut old_letter: char = username_did[0]
         .username
         .to_uppercase()
@@ -160,6 +158,7 @@ fn order_friend_list(
         .next()
         .unwrap();
 
+    // Group friends per first username letter
     for (_friend, is_last_friend) in username_did
         .iter()
         .enumerate()
@@ -167,36 +166,39 @@ fn order_friend_list(
     {
         let first_letter_friend_username = _friend.username.to_uppercase().chars().next().unwrap();
 
-        if old_letter == first_letter_friend_username {
-            group_of_friends_with_same_first_username_letter.push(_friend.clone());
-        } else if !group_of_friends_with_same_first_username_letter.is_empty() {
-            group_of_friends_with_same_first_username_letter
-                .sort_by(|a, b| a.username.to_lowercase().cmp(&b.username.to_lowercase()));
-
-            friends_grouped_per_first_letter.push(FriendListAlpha {
-                first_letter_friends: old_letter,
-                friends: group_of_friends_with_same_first_username_letter.clone(),
-            });
-
+        if old_letter != first_letter_friend_username {
+            sort_friends_and_add_on_friend_list_alpha(
+                &mut group_of_friends_with_same_first_username_letter,
+                &mut friends_grouped_per_first_letter,
+                old_letter,
+            );
             group_of_friends_with_same_first_username_letter = vec![];
-            group_of_friends_with_same_first_username_letter.push(_friend.clone());
         }
 
         old_letter = first_letter_friend_username;
+        group_of_friends_with_same_first_username_letter.push(_friend.clone());
 
         if is_last_friend {
-            if group_of_friends_with_same_first_username_letter.is_empty() {
-                group_of_friends_with_same_first_username_letter.push(_friend.clone());
-            }
-
-            group_of_friends_with_same_first_username_letter
-                .sort_by(|a, b| a.username.to_lowercase().cmp(&b.username.to_lowercase()));
-
-            friends_grouped_per_first_letter.push(FriendListAlpha {
-                first_letter_friends: old_letter,
-                friends: group_of_friends_with_same_first_username_letter.clone(),
-            });
+            sort_friends_and_add_on_friend_list_alpha(
+                &mut group_of_friends_with_same_first_username_letter,
+                &mut friends_grouped_per_first_letter,
+                old_letter,
+            );
         }
     }
     friends_grouped_per_first_letter
+}
+
+fn sort_friends_and_add_on_friend_list_alpha(
+    group_of_friends_with_same_first_username_letter: &mut Vec<UsernameAndDID>,
+    friends_grouped_per_first_letter: &mut Vec<FriendListAlpha>,
+    old_letter: char,
+) {
+    group_of_friends_with_same_first_username_letter
+        .sort_by(|a, b| a.username.to_lowercase().cmp(&b.username.to_lowercase()));
+
+    friends_grouped_per_first_letter.push(FriendListAlpha {
+        first_letter_friends: old_letter,
+        friends: group_of_friends_with_same_first_username_letter.clone(),
+    });
 }
