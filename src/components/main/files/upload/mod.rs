5186b6abbb087fb9e9f4d79e776fe00c09b59558
lgmarchi <lucasgmarchi@gmail.com>
2022-11-17 15:06:45 -0300

    Fix duplicate name problem

--- src/components/main/files/upload/mod.rs
+++ src/components/main/files/upload/mod.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::{path::Path};
 
 use dioxus::{events::MouseEvent, prelude::*};
 use dioxus_heroicons::outline::Shape;
@@ -32,22 +32,53 @@ pub fn Upload<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
                                 None => return
                             };
 
+
+
                             let filename = std::path::Path::new(&file_path)
                             .file_name()
                             .unwrap_or_else(|| std::ffi::OsStr::new(""))
                             .to_str()
                             .unwrap()
                             .to_string();
-
+                            
                             use_future(&cx, &file_storage, |file_storage| async move {
                                 let local_path = Path::new(&file_path).to_string_lossy().to_string();
-
-                                 match file_storage.write().put(&filename, &local_path).await {
-                                    Ok(_) => println!("{:?} file uploaded", &filename),
-                                    Err(error) => println!("Error to upload file: {:?}, error: {:?}", &filename, error),
-                                };
+                                let mut filename_to_save = filename.clone();
+                                let mut count_index_for_duplicate_filename = 1;
+                                loop {
+                                    match file_storage.write().put(&filename_to_save, &local_path).await {
+                                        Ok(_) => {println!("{:?} file uploaded", &filename_to_save); 
+                                        break;
+                                    },
+                                        Err(error) => {
+                                            match &error {
+                                                warp::error::Error::DuplicateName => {
+                                                    let file_name_without_extension = std::path::Path::new(&filename.clone())
+                                                    .with_extension("")
+                                                    .to_str()
+                                                    .unwrap()
+                                                    .to_string();
+    
+                                                    let file_extension = std::path::Path::new(&filename.clone())
+                                                    .extension()
+                                                    .unwrap_or_else(|| std::ffi::OsStr::new(""))
+                                                    .to_str()
+                                                    .unwrap()
+                                                    .to_string();
+    
+                                                    filename_to_save = format!("{} ({}).{}", file_name_without_extension, count_index_for_duplicate_filename, file_extension);
+                                                    println!("Duplicate name, changing file name to {}", &filename_to_save);
+                                                }, 
+                                                _ => {
+                                                    println!("Error to upload file: {:?}, error: {:?}", &filename_to_save, error);
+                                                    break;
+                                                }
+                                            }
+                                            count_index_for_duplicate_filename += 1;
+                                        },
+                                    };
+                                } 
                             });
-
                         }
                     }
                 },
