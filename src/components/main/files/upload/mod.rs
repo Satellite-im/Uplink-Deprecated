2362ee7c56ac907888eb9cfeafc9607974fa1d12
lgmarchi <lucasgmarchi@gmail.com>
2022-11-17 15:51:09 -0300

    Better approach for event handlers is cx.spawn instead of use_future

--- src/components/main/files/upload/mod.rs
+++ src/components/main/files/upload/mod.rs
@@ -1,6 +1,6 @@
 use std::{path::Path};
 
-use dioxus::{events::MouseEvent, prelude::*};
+use dioxus::{events::MouseEvent, prelude::*, core::to_owned};
 use dioxus_heroicons::outline::Shape;
 
 use crate::components::ui_kit::icon_button::IconButton;
@@ -41,43 +41,48 @@ pub fn Upload<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
                             .unwrap()
                             .to_string();
                             
-                            use_future(&cx, &file_storage, |file_storage| async move {
-                                let local_path = Path::new(&file_path).to_string_lossy().to_string();
-                                let mut filename_to_save = filename.clone();
-                                let mut count_index_for_duplicate_filename = 1;
-                                loop {
-                                    match file_storage.write().put(&filename_to_save, &local_path).await {
-                                        Ok(_) => {println!("{:?} file uploaded", &filename_to_save); 
-                                        break;
-                                    },
-                                        Err(error) => {
-                                            match &error {
-                                                warp::error::Error::DuplicateName => {
-                                                    let file_name_without_extension = std::path::Path::new(&filename.clone())
-                                                    .with_extension("")
-                                                    .to_str()
-                                                    .unwrap()
-                                                    .to_string();
-    
-                                                    let file_extension = std::path::Path::new(&filename.clone())
-                                                    .extension()
-                                                    .unwrap_or_else(|| std::ffi::OsStr::new(""))
-                                                    .to_str()
-                                                    .unwrap()
-                                                    .to_string();
-    
-                                                    filename_to_save = format!("{} ({}).{}", file_name_without_extension, count_index_for_duplicate_filename, file_extension);
-                                                    println!("Duplicate name, changing file name to {}", &filename_to_save);
-                                                }, 
-                                                _ => {
-                                                    println!("Error to upload file: {:?}, error: {:?}", &filename_to_save, error);
-                                                    break;
+                            cx.spawn({
+                                to_owned![file_storage, file_path, filename];
+                                async move {
+                                    let local_path = Path::new(&file_path).to_string_lossy().to_string();
+                                    let mut filename_to_save = filename.clone();
+                                    let mut count_index_for_duplicate_filename = 1;
+                                    let mut write_storage = file_storage.write();
+                                    loop {
+                                        match write_storage.put(&filename_to_save, &local_path).await {
+                                            Ok(_) => {
+                                                println!("{:?} file uploaded", &filename_to_save); 
+                                                break;
+                                            },
+                                            Err(error) => {
+                                                match &error {
+                                                    warp::error::Error::DuplicateName => {
+                                                        let file_name_without_extension = std::path::Path::new(&filename.clone())
+                                                        .with_extension("")
+                                                        .to_str()
+                                                        .unwrap()
+                                                        .to_string();
+        
+                                                        let file_extension = std::path::Path::new(&filename.clone())
+                                                        .extension()
+                                                        .unwrap_or_else(|| std::ffi::OsStr::new(""))
+                                                        .to_str()
+                                                        .unwrap()
+                                                        .to_string();
+        
+                                                        filename_to_save = format!("{} ({}).{}", file_name_without_extension, count_index_for_duplicate_filename, file_extension);
+                                                        println!("Duplicate name, changing file name to {}", &filename_to_save);
+                                                    }, 
+                                                    _ => {
+                                                        println!("Error to upload file: {:?}, error: {:?}", &filename_to_save, error);
+                                                        break;
+                                                    }
                                                 }
-                                            }
-                                            count_index_for_duplicate_filename += 1;
-                                        },
-                                    };
-                                } 
+                                                count_index_for_duplicate_filename += 1;
+                                            },
+                                        };
+                                    } 
+                                }
                             });
                         }
                     }
