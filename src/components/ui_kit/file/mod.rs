9c86ff24cc620708e1d09e8801035fdb2cd1f321
lgmarchi <lucasgmarchi@gmail.com>
2022-11-17 13:15:28 -0300

    Format file name when it is to big

--- src/components/ui_kit/file/mod.rs
+++ src/components/ui_kit/file/mod.rs
@@ -19,14 +19,39 @@ pub fn File(cx: Scope<Props>) -> Element {
         State::Secondary => "secondary",
     };
 
+    let file_name = format_file_name_to_show(cx);
+
     cx.render(rsx! {
         div {
             class: "folder {class}",
             Icon { icon: Shape::Document },
-            p { "{cx.props.name}" },
+            p { "{file_name}" },
             label {
                 "{cx.props.size} bytes"
             }
         }
     })
+}
+
+fn format_file_name_to_show(cx: Scope<Props>) -> String {
+    let mut file_name = cx.props.name.clone();
+
+    let file_name_without_extension = std::path::Path::new(&file_name)
+        .with_extension("")
+        .to_str()
+        .unwrap()
+        .to_string();
+
+    if file_name_without_extension.len() > 10 {
+        file_name = match &file_name.get(0..5) {
+            Some(name_sliced) => format!(
+                "{}...{}.{}",
+                name_sliced,
+                file_name_without_extension[file_name_without_extension.len() - 2..].to_string(),
+                cx.props.kind
+            ),
+            None => file_name.clone(),
+        };
+    }
+    file_name
 }
