b51de2f792e99e73e89fb0733b69856160de70b5
lgmarchi <lucasgmarchi@gmail.com>
2022-11-17 12:05:30 -0300

    Upload any file on File System

--- src/components/main/files/upload/mod.rs
+++ src/components/main/files/upload/mod.rs
@@ -26,32 +26,27 @@ pub fn Upload<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
                     input {
                         "type": "file",
                         onclick: move |_| {
-                            // let _p = e.data.value.clone();
-
-
-                            // println!("Evt {:?}", tempVal);
-
-                            // let _getFile = file_storage.read().select("/");
-                            // upload_file.set(_p);
-                            // println!("Evt {:?}", tempVal);
-                            
-                            let file_path = match FileDialog::new().add_filter("image", &["jpg", "png", "jpeg", "svg"]).set_directory(".").pick_file() {
+                                            
+                            let file_path = match FileDialog::new().set_directory(".").pick_file() {
                                 Some(path) => path,
                                 None => return
                             };
-                            let tempVal = &file_storage.read().current_directory();
-
 
                             let local_path = Path::new(&file_path).to_string_lossy().to_string();
 
-                            use_future(&cx, &file_storage, |file_storage| async move {
-                                let mut _upload_file = match file_storage.write().put("/", &local_path).await {
-                                    Ok(_) => println!("Ok"),
-                                    Err(error) => println!("Error {:?}", error),
+                            let filename = std::path::Path::new(&file_path)
+                            .file_name()
+                            .unwrap_or_else(|| std::ffi::OsStr::new(""))
+                            .to_str()
+                            .unwrap()
+                            .to_string();
+
+                            use_future(&cx, (&file_storage, &local_path), |(file_storage, local_path)| async move {
+                                let mut _upload_file = match file_storage.write().put(format!("/{}", filename).as_str(), &local_path).await {
+                                    Ok(success) => println!("File Uploaded: {:?}", success),
+                                    Err(error) => println!("Error upload file: {:?}", error),
                                 };
                             });
-
-                            println!("{:?}", tempVal);
                         }
                     }
                 },
