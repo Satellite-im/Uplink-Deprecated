227ec525a35f9db333f86033e06f21a965554f62
Andre Christoga Pramaditya <caxvis@gmail.com>
2022-11-19 00:13:16 +0700

    chore(rust): remove trailing whitespace

--- src/components/main/files/upload/mod.rs
+++ src/components/main/files/upload/mod.rs
@@ -1,10 +1,10 @@
-use std::{path::Path};
+use std::path::Path;
 
-use dioxus::{events::MouseEvent, prelude::*, core::to_owned};
+use dioxus::{core::to_owned, events::MouseEvent, prelude::*};
 use dioxus_heroicons::outline::Shape;
 
-use ui_kit::icon_button::IconButton;
 use rfd::FileDialog;
+use ui_kit::icon_button::IconButton;
 
 #[derive(Props)]
 pub struct Props<'a> {
@@ -40,7 +40,7 @@ pub fn Upload<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
                             .to_str()
                             .unwrap()
                             .to_string();
-                            
+
                             cx.spawn({
                                 to_owned![file_storage, file_path, filename];
                                 async move {
@@ -62,17 +62,17 @@ pub fn Upload<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
                                                         .to_str()
                                                         .unwrap()
                                                         .to_string();
-        
+
                                                         let file_extension = std::path::Path::new(&filename.clone())
                                                         .extension()
                                                         .unwrap_or_else(|| std::ffi::OsStr::new(""))
                                                         .to_str()
                                                         .unwrap()
                                                         .to_string();
-        
+
                                                         filename_to_save = format!("{} ({}).{}", file_name_without_extension, count_index_for_duplicate_filename, file_extension);
                                                         println!("Duplicate name, changing file name to {}", &filename_to_save);
-                                                    }, 
+                                                    },
                                                     _ => {
                                                         println!("Error to upload file: {:?}, error: {:?}", &filename_to_save, error);
                                                         break;
@@ -81,7 +81,7 @@ pub fn Upload<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
                                                 count_index_for_duplicate_filename += 1;
                                             },
                                         };
-                                    } 
+                                    }
                                 }
                             });
                         }
