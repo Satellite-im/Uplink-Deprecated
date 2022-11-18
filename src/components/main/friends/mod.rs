964181a8e3bd55123249ec4edd502fa533fda89f
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2022-11-18 09:38:58 -0500

    refactor (friends list): use a HashMap to simplify sorting friends list

--- src/components/main/friends/mod.rs
+++ src/components/main/friends/mod.rs
@@ -2,7 +2,10 @@ pub mod friend;
 pub mod request;
 pub mod sidebar;
 
-use std::{collections::HashSet, time::Duration};
+use std::{
+    collections::{HashMap, HashSet},
+    time::Duration,
+};
 
 use crate::{
     components::main::friends::{friend::Friend, sidebar::Sidebar},
@@ -19,7 +22,7 @@ struct UsernameAndDID {
 
 #[derive(PartialEq)]
 struct FriendListAlpha {
-    first_letter_friends: char,
+    letter: char,
     friends: Vec<UsernameAndDID>,
 }
 
@@ -33,30 +36,26 @@ pub struct Props {
 pub fn Friends(cx: Scope<Props>) -> Element {
     log::debug!("rendering Friends");
     let add_error = use_state(&cx, String::new);
-    let friends_grouped_per_first_letter = use_state(&cx, Vec::new);
-    let friends = use_state(&cx, HashSet::new);
+    let disp_friends = use_state(&cx, Vec::new);
+    let friends = use_ref(&cx, HashSet::new);
 
     use_future(
         &cx,
-        (
-            friends,
-            &cx.props.account.clone(),
-            friends_grouped_per_first_letter,
-        ),
-        |(friends, mp, friends_grouped_per_first_letter)| async move {
+        (friends, &cx.props.account.clone(), disp_friends),
+        |(friends, mp, disp_friends)| async move {
             loop {
                 let friends_list: HashSet<_> =
                     HashSet::from_iter(mp.read().list_friends().unwrap_or_default());
 
-                if *friends != friends_list && !friends_list.is_empty() {
+                if *friends.read() != friends_list {
                     log::debug!("updating friends list ");
-                    let new_friends_list =
-                        order_friend_list(&friends_list, &mp).unwrap_or_default();
-                    friends_grouped_per_first_letter.set(new_friends_list);
-                    friends.set(friends_list);
+                    if let Some(new_disp) = order_friend_list(&friends_list, &mp) {
+                        *friends.write_silent() = friends_list;
+                        disp_friends.set(new_disp);
+                    }
                 }
 
-                tokio::time::sleep(Duration::from_millis(300)).await;
+                tokio::time::sleep(Duration::from_millis(1000)).await;
             }
         },
     );
@@ -74,8 +73,8 @@ pub fn Friends(cx: Scope<Props>) -> Element {
                 id: "content",
                 div {
                     class: "friends-list",
-                    friends_grouped_per_first_letter.iter().map(|friends_per_char_list| {
-                        let first_username_char = friends_per_char_list.first_letter_friends;
+                    disp_friends.iter().map(|friends_per_char_list| {
+                        let first_username_char = friends_per_char_list.letter;
                         rsx!(
                             div {
                                 class: "friends-separator",
@@ -117,71 +116,40 @@ fn order_friend_list(
     friend_did_list: &HashSet<warp::crypto::DID>,
     account: &Account,
 ) -> Option<Vec<FriendListAlpha>> {
-    let mut username_did: Vec<UsernameAndDID> = Vec::new();
-    let mut group_of_friends_with_same_first_username_letter: Vec<UsernameAndDID> = Vec::new();
-    let mut friends_grouped_per_first_letter: Vec<FriendListAlpha> = Vec::new();
-
-    // Get all friends username
-    for friend_did in friend_did_list.iter() {
-        let _friend_username = utils::get_username_from_did(friend_did.clone(), account);
-        let _friend_username_and_did = UsernameAndDID {
-            username: _friend_username,
-            did: friend_did.clone(),
-        };
-        username_did.push(_friend_username_and_did);
-    }
-
-    username_did.sort_by(|a, b| a.username.cmp(&b.username));
-
-    // Get the first letter username that start the list
-    let mut old_letter: char = username_did
-        .first()?
-        .username
-        .to_uppercase()
-        .chars()
-        .next()?;
-
-    // Group friends per first username letter
-    for (_friend, is_last_friend) in username_did
+    // Get all friends username. sort them
+    let mut total_friends_list: Vec<UsernameAndDID> = friend_did_list
         .iter()
-        .enumerate()
-        .map(|(i, f)| (f, i == username_did.len() - 1))
-    {
-        let first_letter_friend_username = _friend.username.to_uppercase().chars().next()?;
+        .map(|did| {
+            let _friend_username = utils::get_username_from_did(did.clone(), account);
+            UsernameAndDID {
+                username: _friend_username,
+                did: did.clone(),
+            }
+        })
+        .collect();
+    total_friends_list.sort_by(|a, b| a.username.cmp(&b.username));
 
-        if old_letter != first_letter_friend_username {
-            sort_friends_and_add_on_friend_list_alpha(
-                &mut group_of_friends_with_same_first_username_letter,
-                &mut friends_grouped_per_first_letter,
-                old_letter,
-            );
-            group_of_friends_with_same_first_username_letter.clear();
-        }
-
-        old_letter = first_letter_friend_username;
-        group_of_friends_with_same_first_username_letter.push(_friend.clone());
-
-        if is_last_friend {
-            sort_friends_and_add_on_friend_list_alpha(
-                &mut group_of_friends_with_same_first_username_letter,
-                &mut friends_grouped_per_first_letter,
-                old_letter,
-            );
-        }
+    // split by letter. the vectors will be sorted already
+    let mut sublists: HashMap<char, Vec<UsernameAndDID>> = HashMap::new();
+    for friend in total_friends_list {
+        let start = friend.username.to_uppercase().chars().next()?;
+        match sublists.get_mut(&start) {
+            Some(list) => list.push(friend),
+            None => {
+                let _ = sublists.insert(start, vec![friend]);
+            }
+        };
     }
-    Some(friends_grouped_per_first_letter)
-}
+    // turn into a vec and sort it.
+    let mut ordered_sublists: Vec<FriendListAlpha> = sublists
+        .iter()
+        .map(|(k, v)| FriendListAlpha {
+            letter: *k,
+            friends: v.clone(),
+        })
+        .collect();
 
-fn sort_friends_and_add_on_friend_list_alpha(
-    group_of_friends_with_same_first_username_letter: &mut Vec<UsernameAndDID>,
-    friends_grouped_per_first_letter: &mut Vec<FriendListAlpha>,
-    old_letter: char,
-) {
-    group_of_friends_with_same_first_username_letter
-        .sort_by(|a, b| a.username.to_lowercase().cmp(&b.username.to_lowercase()));
+    ordered_sublists.sort_by(|l, r| l.letter.cmp(&r.letter));
 
-    friends_grouped_per_first_letter.push(FriendListAlpha {
-        first_letter_friends: old_letter,
-        friends: group_of_friends_with_same_first_username_letter.clone(),
-    });
+    Some(ordered_sublists)
 }
