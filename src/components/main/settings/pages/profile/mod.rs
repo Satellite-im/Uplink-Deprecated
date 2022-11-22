548a36597b11f814d4ecc8b12d312929fb765b9e
Yijing Huang <hyj891204@gmail.com>
2022-11-22 01:07:45 -0600

    refactor(Profile):refactor update_user_name function

--- src/components/main/settings/pages/profile/mod.rs
+++ src/components/main/settings/pages/profile/mod.rs
@@ -8,7 +8,7 @@ use ui_kit::{
     input::Input,
     photo_picker::PhotoPicker,
 };
-use warp::multipass::identity::{IdentityUpdate, Identifier};
+use warp::multipass::identity::{IdentityUpdate};
 
 #[derive(Props, PartialEq)]
 pub struct Props {
@@ -26,6 +26,7 @@ pub fn Profile(cx: Scope<Props>, account:Account) -> Element {
     let identity = account2.read().get_own_identity().unwrap();
 
     let user_name = identity.username();
+    let user_name2 = user_name.clone();
     let user_name_state = use_state(&cx, || user_name.clone());
     let edit_user_name_state = use_state(&cx, || false);
     let user_name_error = use_state(&cx, String::new);
@@ -43,19 +44,51 @@ pub fn Profile(cx: Scope<Props>, account:Account) -> Element {
     let status_msg2 = status_msg.clone();
     let status_msg_state = use_state(&cx, || status_msg.clone());
     let edit_status_msg_state = use_state(&cx, || false);
+    let status_msg_error = use_state(&cx, String::new);
+    let status_msg_error_class = if status_msg_error.is_empty() {
+        css!("opacity: 0")
+    } else {
+            "error_text"
+        };
 
-    let update_user_name = move |_| {
-        // user name can't be none
-        // unfinished
-        edit_user_name_state.set(false);
-    };
+
+    fn update_user_name(user_name_state:&UseState<String>, user_name:&String,user_name_error:&UseState<String>,account:&Account,edit_user_name_state:&UseState<bool>){
+        let user_name_text = user_name_state.trim();
+        if user_name_text != *user_name{
+                if user_name_text.is_empty(){
+                        user_name_error.set("Username is required".into())
+                }else if user_name_text.len() < 4 ||user_name_text.len() > 32  {
+                        user_name_error.set("Username needs to be between 4 and 32 characters long".into())
+                }else{
+                let user_name_regex_set = RegexSet::new(&[
+                            r"@",
+                            r"\p{Emoji_Presentation}",
+                 ]).unwrap();
+                let matches = user_name_regex_set.matches(user_name_text);
+                if matches.matched(0){
+                    user_name_error.set("@ is not allowed in username".into());
+                } else if matches.matched(1){
+                    user_name_error.set("emoji is not allowed in username".into());    
+                } else {
+                    if let Err(e) =  account.write().update_identity(IdentityUpdate::set_username(
+                        user_name_text.to_string()))
+                        {
+                        println!("Failed in updating status message:{}", e);
+                        }
+                        edit_user_name_state.set(false);
+                }
+            }
+        }
+
+    }
 
 
-    fn update_status_msg(status_msg_state:&UseState<String>,status_msg: &String,user_name_error: &UseState<String>, account:&Account, edit_status_msg_state:&UseState<bool> ){
+
+    fn update_status_msg(status_msg_state:&UseState<String>,status_msg: &String,status_msg_error: &UseState<String>, account:&Account, edit_status_msg_state:&UseState<bool> ){
          let status_msg_text = status_msg_state.trim();
                                     if status_msg_text != *status_msg{
                                         if status_msg_text.len() > 128{
-                                            user_name_error.set("status message needs to be less than 128 characters".into());
+                                            status_msg_error.set("status message needs to be less than 128 characters".into());
 
                                         }else{
                                             if let Err(e) =  account.write().update_identity(IdentityUpdate::set_status_message(
@@ -100,48 +133,15 @@ pub fn Profile(cx: Scope<Props>, account:Account) -> Element {
                                         user_name_state.set(e.value.clone());
                                     },
                                     on_enter:move|_|{
-                                let user_name_text = user_name_state.trim();
-
-                                if user_name_text != user_name{
-                                        if user_name_text.is_empty(){
-                                                user_name_error.set("Username is required".into())
-                                        }else if user_name_text.len() < 4 ||user_name_text.len() > 32  {
-                                                user_name_error.set("Username needs to be between 4 and 32 characters long".into())
-                                        }else{
-                                        let user_name_regex_set = RegexSet::new(&[
-                                                    r"@",
-                                                    r"\p{Emoji}",
-                                         ]).unwrap();
-                                                    
-                                        let matches: Vec<_> = user_name_regex_set.matches(user_name_text).into_iter().collect();
-
-                                        if matches.contains(&0){
-                                            user_name_error.set("@ is not allowed in username".into());
-                                        } else if matches.contains(&1){
-                                            user_name_error.set("emoji is not allowed in username".into());
-                                        } else {
-                                        //TODO: check if username already exists
-                                          let return_result =  account.write().get_identity(Identifier::user_name(user_name_text));
-
-                                         match return_result {
-                                            Ok(user) => println!("user: {:?}",user.iter()),
-                                            Err(e) => println!("error: {}",e),
-                                         }
-                                            // if let Err(e) =  account.write().update_identity(IdentityUpdate::set_username(
-                                            //     user_name_text.to_string()))
-                                            //     {
-                                            //     println!("Failed in updating status message:{}", e);
-                                            //     }
-                                            //     edit_user_name_state.set(false);
-                                        }
-                                    }
-                                }
+                                        update_user_name(user_name_state,&user_name,user_name_error,account,edit_user_name_state);
                             },
                         },
                     },
                     Button {
                                 text: l.save.to_string(),
-                                on_pressed: update_user_name
+                                on_pressed: move |_|{
+                                    update_user_name(user_name_state,&user_name2,user_name_error,account,edit_user_name_state);
+                                }
                             }
                         },
                     )} else {rsx! (
@@ -204,7 +204,11 @@ pub fn Profile(cx: Scope<Props>, account:Account) -> Element {
                                 },
                             },
                         },)
-                }}
+                }
+                p {
+                    class: "{status_msg_error_class}",
+                    "{status_msg_error}"
+                },}
             },
         }
     })
