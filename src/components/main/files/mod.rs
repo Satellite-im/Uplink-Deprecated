82976e9d822a9633c07a6b22ba1a178862fc838e
Nathan-Power <nathan.c.power@icloud.com>
2022-11-23 20:10:11 +1100

    feat(settings): configure layout responsiveness for settings page

--- src/components/main/files/mod.rs
+++ src/components/main/files/mod.rs
@@ -7,6 +7,7 @@ use crate::{
         reusable::nav::Nav,
     },
     main::files::{browser::FileBrowser, toolbar::Toolbar, upload::Upload},
+    STATE,
 };
 pub mod browser;
 pub mod sidebar;
@@ -24,9 +25,16 @@ pub fn Files(cx: Scope<Props>) -> Element {
     let show_new_folder = use_state(&cx, || false);
     let show_upload = use_state(&cx, || false);
 
+    let st = use_atom_ref(&cx, STATE).clone();
+    let sidebar_visibility = match st.read().hide_sidebar {
+        false => "sidebar-visible",
+        true => "sidebar-hidden",
+    };
+
     cx.render(rsx! {
         div {
             id: "files",
+            class: "{sidebar_visibility}",
             sidebar::Sidebar { account: cx.props.account.clone() },
             div {
                 id: "content",
--- src/components/main/settings/mod.rs
+++ src/components/main/settings/mod.rs
@@ -4,8 +4,13 @@ use crate::{
     components::main::settings::pages::{
         developer::Developer, extensions::Extensions, general::General, profile::Profile,
     },
-    Account,
+    components::reusable::toolbar,
+    state::Actions,
+    Account, STATE,
 };
+
+use dioxus_heroicons::outline::Shape;
+use ui_kit::icon_button::IconButton;
 
 use self::sidebar::nav::Route;
 
@@ -20,6 +25,7 @@ pub struct Props {
 
 #[allow(non_snake_case)]
 pub fn Settings(cx: Scope<Props>) -> Element {
+    let st = use_atom_ref(&cx, STATE).clone();
     log::debug!("rendering Settings");
     let page_to_open_on_settings = match cx.props.page_to_open {
         Route::Profile => Route::Profile,
@@ -27,15 +33,33 @@ pub fn Settings(cx: Scope<Props>) -> Element {
         _ => Route::General,
     };
 
+    let sidebar_visibility = match st.read().hide_sidebar {
+        false => "sidebar-visible",
+        true => "sidebar-hidden",
+    };
+
     let active_page = use_state(&cx, || page_to_open_on_settings);
+
+    let active_page_string = match **active_page {
+        Route::Profile => "Profile",
+        Route::Privacy => "Privacy",
+        Route::AudioVideo => "Audio Video",
+        Route::Extensions => "Extensions",
+        Route::Developer => "Developer",
+        _ => "General",
+    };
 
     cx.render(rsx! {
         div {
             id: "settings",
+            class: "{sidebar_visibility}",
             sidebar::SettingsSidebar {
                 account: cx.props.account.clone(),
                 on_pressed: move |ne| {
                     active_page.set(ne);
+
+                    let state = use_atom_ref(&cx, STATE).clone();
+                    state.write().dispatch(Actions::HideSidebar(true));
                 },
                 initial_value: match active_page.get() {
                     Route::Profile => Route::Profile,
@@ -45,6 +69,37 @@ pub fn Settings(cx: Scope<Props>) -> Element {
             },
             div {
                 id: "content",
+                div {
+                    class: "toolbar-wrapper",
+                    toolbar::Toolbar {
+                        controls: cx.render(rsx! {
+                            div {}
+                        }),
+                        div {
+                            class: "toolbar-content",
+                            div {
+                                class: "toolbar-start",
+                                div {
+                                    class: "mobile-back-button",
+                                    IconButton {
+                                        icon: Shape::ArrowLeft,
+                                        state: ui_kit::icon_button::State::Secondary,
+                                        on_pressed: move |_| {
+                                            let state = use_atom_ref(&cx, STATE).clone();
+                                            state.write().dispatch(Actions::HideSidebar(false));
+                                        },
+                                    },
+                                },
+                            },
+                            h1 {
+                                "{active_page_string}",
+                            },
+                            div {
+                                class:  "toolbar-end",
+                            }
+                        }
+                    },
+                },
                 div {
                     id: "page",
                     div {
