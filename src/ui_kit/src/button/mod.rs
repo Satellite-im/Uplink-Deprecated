5f49e7be982c25c1c99dbba4c51b8bbdce64f833
Jason Woodland <me@jasonwoodland.com>
2022-11-29 15:00:38 +1100

    refactor(uikit): add iconbutton functionality to button component

--- src/ui_kit/src/button/mod.rs
+++ src/ui_kit/src/button/mod.rs
@@ -4,9 +4,10 @@ use dioxus_heroicons::{outline::Shape, Icon};
 #[derive(PartialEq, Eq)]
 pub enum State {
     Primary,
+    Secondary,
     Success,
     Danger,
-    Secondary,
+    Filled,
     Transparent,
 }
 
@@ -14,7 +15,6 @@ pub enum State {
 pub struct Props<'a> {
     on_pressed: EventHandler<'a, MouseEvent>,
     #[props(optional)]
-    /// Text to be displayed within the button
     text: Option<String>,
     #[props(optional)]
     icon: Option<Shape>,
@@ -37,41 +37,39 @@ pub fn Button<'a>(cx: Scope<'a, Props>) -> Element<'a> {
 
     let mut class = String::from("button ");
     class += match cx.props.large {
-        Some(_) => "button-lg",
+        Some(_) => "button-lg ",
         None => "",
     };
     class += match cx.props.state.as_ref() {
         Some(state) => match state {
+            State::Secondary => "button-secondary",
             State::Success => "button-success",
             State::Danger => "button-danger",
-            State::Secondary => "button-secondary",
+            State::Filled => "button-filled",
             State::Transparent => "button-transparent",
-            _ => " ",
+            _ => "",
         },
         None => "",
     };
+    // add class if text length is 0
+    if text.len() == 0 {
+        class += " button-icon-only";
+    }
 
-    cx.render(match cx.props.icon {
-        Some(icon) => rsx! {
-            button {
-                class: "{class}",
-                onclick: move |evt| cx.props.on_pressed.call(evt),
-                disabled: "{disabled}",
-                Icon {
-                    icon: icon,
+    cx.render(rsx! {
+        button {
+            class: "{class}",
+            disabled: "{disabled}",
+            onclick: move |evt| cx.props.on_pressed.call(evt),
+            cx.render(match cx.props.icon {
+                Some(icon) => rsx! {
+                    Icon {
+                        icon: icon,
+                    }
                 },
-                span {
-                    "{text}"
-                }
-            }
-        },
-        None => rsx! {
-            button {
-                class: "{class}",
-                disabled: "{disabled}",
-                onclick: move |evt| cx.props.on_pressed.call(evt),
-                "{text}"
-            }
-        },
+                None => rsx! {Fragment()},
+            }),
+            "{text}"
+        }
     })
 }
