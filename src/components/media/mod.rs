f9cb2ecb327ea96e175190695158ff879b17b88b
Jason Woodland <me@jasonwoodland.com>
2022-12-02 13:57:01 +1100

    feat(call): resize call media ui

--- src/components/media/mod.rs
+++ src/components/media/mod.rs
@@ -17,6 +17,41 @@ pub struct Props {
     account: Account,
 }
 
+pub enum Direction {
+    Horizontal,
+    Vertical,
+}
+
+#[derive(Props)]
+pub struct ResizeProps<'a> {
+    direction: Direction,
+    children: Element<'a>,
+}
+
+#[allow(non_snake_case)]
+pub fn ResizeContainer<'a>(cx: Scope<'a, ResizeProps<'a>>) -> Element<'a> {
+    let class = format!(
+        "resize-container {}",
+        match cx.props.direction {
+            Direction::Horizontal => "horizontal",
+            Direction::Vertical => "vertical",
+        }
+    );
+
+    let script = include_str!("resize.js");
+
+    cx.render(rsx! {
+        div {
+            class: "{class}",
+            &cx.props.children,
+            div {
+                class: "resize-handle",
+            }
+            script { "{script}" }
+        }
+    })
+}
+
 #[allow(non_snake_case)]
 pub fn MediaContainer(cx: Scope<Props>) -> Element {
     log::debug!("rendering Media Container");
@@ -36,44 +71,47 @@ pub fn MediaContainer(cx: Scope<Props>) -> Element {
     let script = include_str!("responsive.js");
 
     cx.render(rsx! {
-        div {
-            id: "media-container",
-            class: "{class}",
+        ResizeContainer {
+            direction: Direction::Vertical,
             div {
-                class: "media-view",
+                id: "media-container",
+                class: "{class}",
                 div {
-                    class: "settings-toggle",
-                    Button {
-                        icon: Shape::Cog,
-                        state: ui_kit::button::State::Transparent,
-                        on_pressed: move |_| {},
-                    }
-                },
-                div {
-                    id: "media-content",
-                    names.iter().map(|name| rsx!(
-                        Media {
-                            name: name.to_string(),
-                            src: "".to_string()
+                    class: "media-view",
+                    div {
+                        class: "settings-toggle",
+                        Button {
+                            icon: Shape::Cog,
+                            state: ui_kit::button::State::Transparent,
+                            on_pressed: move |_| {},
                         }
-                    ))
-                },
-                div {
-                    class: "media-toggle",
-                    Button {
-                        icon: if **fullscreen { Shape::ArrowsPointingIn } else { Shape::ArrowsPointingOut },
-                        state: ui_kit::button::State::Transparent,
-                        on_pressed: move |_| fullscreen.set(!fullscreen),
-                    }
-                },
-            }
-            Controls {}
-            script { "{script}" }
-            config.audiovideo.call_timer.then(|| rsx!{
-                Time {
-                    start_time: 0
+                    },
+                    div {
+                        id: "media-content",
+                        names.iter().map(|name| rsx!(
+                            Media {
+                                name: name.to_string(),
+                                src: "".to_string()
+                            }
+                        ))
+                    },
+                    div {
+                        class: "media-toggle",
+                        Button {
+                            icon: if **fullscreen { Shape::ArrowsPointingIn } else { Shape::ArrowsPointingOut },
+                            state: ui_kit::button::State::Transparent,
+                            on_pressed: move |_| fullscreen.set(!fullscreen),
+                        }
+                    },
                 }
-            })
+                Controls {}
+                script { "{script}" }
+                config.audiovideo.call_timer.then(|| rsx!{
+                    Time {
+                        start_time: 0
+                    }
+                })
+            }
         }
     })
 }
