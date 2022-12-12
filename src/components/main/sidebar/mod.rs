a67c46ecb76c64641f4b8d7633e110528b0ca8a4
Nathan-Power <nathan.c.power@icloud.com>
2022-12-12 20:32:18 +1100

    feat(popout): create reusable popout component

--- src/components/main/sidebar/mod.rs
+++ src/components/main/sidebar/mod.rs
@@ -57,7 +57,11 @@ pub fn Sidebar(cx: Scope<Props>) -> Element {
     }
 
     let ext_enabled = state.read().enabled_extensions.clone();
-    let exts = get_renders(ExtensionType::SidebarWidget, config.extensions.enable, ext_enabled);
+    let exts = get_renders(
+        ExtensionType::SidebarWidget,
+        config.extensions.enable,
+        ext_enabled,
+    );
 
     let notifications_tx = use_coroutine(&cx, |mut rx: UnboundedReceiver<Message>| async move {
         while let Some(msg) = rx.next().await {
@@ -195,8 +199,9 @@ pub fn Sidebar(cx: Scope<Props>) -> Element {
                                     is_active: active_chat == Some(conversation_info.conversation.id()),
                                     tx_chan: notifications_tx.clone(),
                                     on_pressed: move |uuid| {
-                                        // on press, change state so CSS class flips to show the chat
-                                        //state.write().dispatch(Actions::HideSidebar(true));
+                                        // hide the sidebar if it's visible for mobile view
+                                        state.write().dispatch(Actions::HideSidebar(true));
+
                                         if *active_chat != Some(uuid) {
                                             state.write().dispatch(Actions::ShowConversation(conversation_info.conversation.id()));
                                             active_chat.set(Some(uuid));
